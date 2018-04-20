40e1a50f197f141f48a525cc05d50fa6d6b27112
Cali Castle <cali@calicastle.com>
2018-04-20 02:03:31 -0700

    Setup footer structure

--- resources/views/bases/main_transparent.blade.php
+++ resources/views/bases/main_transparent.blade.php
@@ -50,6 +50,37 @@
 		</div>
 	</div>
 </section>
+<footer class="ui-footer">
+	<svg class="ui-footer__curve" viewBox="0 0 1440 97" preserveAspectRatio="none">
+		<g id="footer-curve">
+			<path d="M0,0 C143.702507,88.7542517 333.547888,114.816217 569.536145,78.1858968 C805.524401,41.5555762 1095.67902,15.4936106 1440,0 L1440,96.0569821 L0,96.0569821 L0,0 Z"></path>
+		</g>
+	</svg>
+	<div class="ui-footer__main">
+		<div class="ui-footer__connect">
+			<a href="#" class="ui-footer__logo">
+				<img src="/assets/images/logo-white.png" alt="Logo">
+			</a>
+			<span>Connect with us</span>
+			<div class="ui-footer__socials">
+				<a class="ui-footer__social instagram" href="#"></a>
+				<a class="ui-footer__social twitter" href="#"></a>
+				<a class="ui-footer__social facebook" href="#"></a>
+			</div>
+		</div>
+		<div class="ui-footer__related">
+			<div class="ui-footer__links">
+				{{--<h3 class="ui-footer__links__heading">Resources</h3>--}}
+			</div>
+		</div>
+		<div class="ui-footer__international"></div>
+	</div>
+	<div class="ui-footer__separator"></div>
+	<div class="ui-footer__coaches-counter"></div>
+	<div class="ui-footer__copyright">
+		<span>Copyright &copy; {{ date('Y') }} - {{ config('app.name') }}. All rights reserved.</span>
+	</div>
+</footer>
 @stop
 
 @push('body.scripts')
